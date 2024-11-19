df1e9410dd1921b460ee0b2f3f281083a42d369a
Morgan-deve <mohammad.mrg.salehi@gmail.com>
2024-11-19 18:36:20 +0330

    ok

--- knn.py
+++ knn.py
@@ -1,22 +1,23 @@
 from sklearn.datasets import fetch_openml
-from numpy import dot, linalg
-from pandas import DataFrame
+from numpy import dot, linalg, mean
+from pandas import DataFrame , concat
 
 
 mnist = fetch_openml('mnist_784')
-mnist_data: DataFrame = mnist.data[:10000]
-mnist_target: DataFrame = mnist.target[:10000]
+mnist_data: DataFrame = mnist.data[:1000]
+mnist_target: DataFrame = mnist.target[:1000]
 
-trainset = mnist_data[:10000 * 8//10]
-testset = mnist_data[10000 * 8//10:]
+trainset = mnist_data[:1000 * 8//10]
+testset = mnist_data[1000 * 8//10:]
 
+kfold = 10
 
 def metric(q,p):
     innerproduct = dot(q,p)
     magnitudeproduct = linalg.norm(q) * linalg.norm(p)
     return innerproduct / magnitudeproduct
 
-def predict(x, k: int, trainset):
+def predict(x: DataFrame, trainset: DataFrame, k: int):
     neighbors = []
     
     for i in range(len(trainset.index)):
@@ -39,3 +40,34 @@ def predict(x, k: int, trainset):
     return str(lable[0])
 
 
+def kaccuracy(k: int):
+    accuracy = []
+    for i in range(kfold):
+        firstindex = i *(len(trainset.index)//kfold)
+        lastindex = (i+1)*(len(trainset.index)//kfold)
+        
+        kfoldset = trainset[firstindex: lastindex]
+        newtrainset = concat([trainset[0: firstindex], trainset[lastindex:]])
+        
+        rightclassification = 0
+        for i in range(len(kfoldset.index)):
+            predictlable = predict(kfoldset.iloc[i], newtrainset, k)
+            reallable = mnist_target[kfoldset.index[i]]
+            if (predictlable == reallable):
+                rightclassification += 1
+        
+        accuracy.append(float('%.2f' % (rightclassification / len(kfoldset.index))))
+        
+    return float('%.2f' % mean(accuracy))
+
+print (f"Accuracy for k=1: {kaccuracy(1)}")
+print (f"Accuracy for k=2: {kaccuracy(2)}")
+print (f"Accuracy for k=3: {kaccuracy(3)}")
+print (f"Accuracy for k=4: {kaccuracy(4)}")
+print (f"Accuracy for k=5: {kaccuracy(5)}")
+print (f"Accuracy for k=6: {kaccuracy(6)}")
+print (f"Accuracy for k=7: {kaccuracy(7)}")
+print (f"Accuracy for k=8: {kaccuracy(8)}")
+print (f"Accuracy for k=9: {kaccuracy(9)}")
+print (f"Accuracy for k=10: {kaccuracy(10)}")
+        
